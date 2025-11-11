af4fcc1e724d3432ae842284b9037727abe584e6
liusir <16197701+liuyp2077@user.noreply.gitee.com>
2025-11-11 23:49:59 +0800

    add test.cpp

---
+++ AppScope/test.h
@@ -0,0 +1,14 @@
+//
+// Created on 2025/11/11.
+//
+// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
+// please include "napi/native_api.h".
+
+#ifndef SUPER_TEST_H
+#define SUPER_TEST_H
+
+class test {
+
+};
+
+#endif //SUPER_TEST_H
